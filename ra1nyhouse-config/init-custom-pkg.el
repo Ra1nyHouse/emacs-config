8ea2061ce068392fe49edc4802d9667d0f272afb
Ra1nyHouse <452588680@qq.com>
2017-06-12 01:54:47 +0800

    update copypaste

--- ra1nyhouse-config/init-custom-pkg.el
+++ ra1nyhouse-config/init-custom-pkg.el
@@ -170,6 +170,8 @@
 ;; program for getting and setting the contents of the X selection"
 (unless window-system
   (when (getenv "DISPLAY")
+    ;; 获取平台名字"Linux" or "Darwin"
+    (defvar my-platform (shell-command-to-string "uname"))
     ;; Callback for when user cuts
     (defun xsel-cut-function (text &optional push)
       ;; Insert text to temp-buffer, and "send" content to xsel stdin
@@ -178,14 +180,30 @@
 	;; I prefer using the "clipboard" selection (the one the
 	;; typically is used by c-c/c-v) before the primary selection
 	;; (that uses mouse-select/middle-button-click)
-	(call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")))
+	(if (equal my-platform "Linux\n")
+	    ;; linux 平台
+	    (call-process-region (point-min) (point-max) "xsel" nil 0 nil "--clipboard" "--input")
+	    ;; mac平台
+	    (call-process-region (point-min) (point-max) "pbcopy" nil 0 nil "")
+	  )
+       ))
+      
     ;; Call back for when user pastes
     (defun xsel-paste-function()
       ;; Find out what is current selection by xsel. If it is different
       ;; from the top of the kill-ring (car kill-ring), then return
       ;; it. Else, nil is returned, so whatever is in the top of the
       ;; kill-ring will be used.
-      (let ((xsel-output (shell-command-to-string "xsel --clipboard --output")))
+      (let (
+	    (xsel-output
+	     (if (equal my-platform "Linux\n")
+		 ;; Linux
+		 (shell-command-to-string "xsel --clipboard --output")
+	         ;; mac
+	         (shell-command-to-string "pbpaste")
+	       )
+	     )
+	    )
 	(unless (string= (car kill-ring) xsel-output)
 	  xsel-output )))
     ;; Attach callbacks to hooks
